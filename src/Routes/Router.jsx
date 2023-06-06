ffaa37fed2bf9ffb2682191882357923373abdd7
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2023-06-07 01:03:24 +0600

    404 page added

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
+import Errorpage from "../Pages/Errorpage/Errorpage";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <MainLayout></MainLayout>,
+        errorElement: <Errorpage></Errorpage>,
         children: [
             
                 {
